c3d5b8ebabb5a9c9da2071571363e9f1e618f397
Braian O. Dias <braian.d@gmail.com>
2022-06-08 23:31:30 +0300

    added resnet34 baseline

--- brightfield.py
+++ brightfield.py
@@ -10,14 +10,13 @@ from urllib.request import urlretrieve
 
 
 class BrightfieldDataset(torch.utils.data.Dataset):
-    def __init__(self, root, mode="train", masks_directory='complete_stencils', mask_threshold=0.01, split_file="", transform=None):
+    def __init__(self, root, mode="train", masks_directory='complete_stencils', split_file="", transform=None):
 
         assert mode in {"train", "val", "test"}
 
         self.root = root
         self.mode = mode
         self.transform = transform
-        self.mask_threshold = mask_threshold
 
         self.images_directory = os.path.join(self.root, "images")
         self.masks_directory = os.path.join(self.root, masks_directory)
@@ -32,14 +31,13 @@ class BrightfieldDataset(torch.utils.data.Dataset):
         image_filename, mask_filename = self.filenames[idx]
         image_path = os.path.join(self.images_directory, image_filename[image_filename.rfind("/")+1:])
         mask_path = os.path.join(self.masks_directory, mask_filename[mask_filename.rfind("/")+1:])
-        # print(mask_path)
 
         image = Image.open(image_path).resize((256, 256), Image.LINEAR)
         mask = Image.open(mask_path).resize((256, 256), Image.LINEAR)
 
         image = np.array(image)
         mask = np.array(mask)
-        mask = self._preprocess_mask(mask, self.mask_threshold)
+        mask = self._preprocess_mask(mask)
         
         # convert to other format HWC -> CHW
         image = np.moveaxis(image, -1, 0)
@@ -55,13 +53,10 @@ class BrightfieldDataset(torch.utils.data.Dataset):
         return sample
 
     @staticmethod
-    def _preprocess_mask(mask, threshold):     
-        mask = mask.astype(np.float32)           
-        norm_mask = mask - np.min(mask) 
-        norm_mask = norm_mask / (np.max(mask) - np.min(mask))
-
-        mask[mask<=threshold] = 0.0
-        mask[mask>threshold] = 1.0
+    def _preprocess_mask(mask):     
+        mask = mask.astype(np.float32)
+        # make all cell classes being represented by the same class (1) and background is 0
+        mask[mask>0] = 1.0
         
         return mask
 
@@ -71,70 +66,4 @@ class BrightfieldDataset(torch.utils.data.Dataset):
 
         return filenames
 
-    # @staticmethod
-    # def download(root):
-
-    #     # load images
-    #     filepath = os.path.join(root, "images.tar.gz")
-    #     download_url(
-    #         url="https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz",
-    #         filepath=filepath,
-    #     )
-    #     extract_archive(filepath)
-
-    #     # load annotations
-    #     filepath = os.path.join(root, "annotations.tar.gz")
-    #     download_url(
-    #         url="https://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz",
-    #         filepath=filepath,
-    #     )
-    #     extract_archive(filepath)
-
-
-# class SimpleOxfordPetDataset(OxfordPetDataset):
-#     def __getitem__(self, *args, **kwargs):
-
-#         sample = super().__getitem__(*args, **kwargs)
-
-#         # resize images
-#         image = np.array(Image.fromarray(sample["image"]).resize((256, 256), Image.LINEAR))
-#         mask = np.array(Image.fromarray(sample["mask"]).resize((256, 256), Image.NEAREST))
-#         trimap = np.array(Image.fromarray(sample["trimap"]).resize((256, 256), Image.NEAREST))
-
-#         # convert to other format HWC -> CHW
-#         sample["image"] = np.moveaxis(image, -1, 0)
-#         sample["mask"] = np.expand_dims(mask, 0)
-#         sample["trimap"] = np.expand_dims(trimap, 0)
-
-#         return sample
-
-
-# class TqdmUpTo(tqdm):
-#     def update_to(self, b=1, bsize=1, tsize=None):
-#         if tsize is not None:
-#             self.total = tsize
-#         self.update(b * bsize - self.n)
-
-
-# def download_url(url, filepath):
-#     directory = os.path.dirname(os.path.abspath(filepath))
-#     os.makedirs(directory, exist_ok=True)
-#     if os.path.exists(filepath):
-#         return
-
-#     with TqdmUpTo(
-#         unit="B",
-#         unit_scale=True,
-#         unit_divisor=1024,
-#         miniters=1,
-#         desc=os.path.basename(filepath),
-#     ) as t:
-#         urlretrieve(url, filename=filepath, reporthook=t.update_to, data=None)
-#         t.total = t.n
-
-
-# def extract_archive(filepath):
-#     extract_dir = os.path.dirname(os.path.abspath(filepath))
-#     dst_dir = os.path.splitext(filepath)[0]
-#     if not os.path.exists(dst_dir):
-#         shutil.unpack_archive(filepath, extract_dir)
+    
